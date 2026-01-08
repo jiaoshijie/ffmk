f3a8a2ac84327f73fbe61942136609ae9cffbffe
jiaoshijie <jiao1751959040@gmail.com>
2026-01-08 21:40:38 +0800

    feat: lazy loading

--- lua/ffmk/init.lua
+++ lua/ffmk/init.lua
@@ -1,32 +1,36 @@
 local _M = {}
-local rt = require('ffmk.runtime')
 
 --- @param cfg table? { ui = {}, cmd = {} }
 _M.files = function(cfg)
+    local rt = require('ffmk.runtime')
     if not rt.setup("files", cfg, { rg = true, fd = true }) then return end
     rt.run()
 end
 
 --- @param cfg table? { ui = {}, cmd = {} }
 _M.grep = function(cfg)
+    local rt = require('ffmk.runtime')
     if not rt.setup("grep", cfg, { rg = true }) then return end
     rt.run()
 end
 
 --- @param cfg table? { ui = {}, cmd = {} }
 _M.helptags = function(cfg)
+    local rt = require('ffmk.runtime')
     if not rt.setup("helptags", cfg, nil) then return end
     rt.run()
 end
 
 --- @param cfg table? { ui = {}, cmd = {} }
 _M.ctags = function(cfg)
+    local rt = require('ffmk.runtime')
     if not rt.setup("ctags", cfg, { ctags = true }) then return end
     rt.run()
 end
 
 --- @param cfg table? { ui = {}, cmd = {} }
 _M.gnu_global = function(cfg)
+    local rt = require('ffmk.runtime')
     if not rt.setup("gnu_global", cfg, { gnu_global = true }) then return end
     rt.run()
 end
